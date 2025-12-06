3752441c357448bf82d0c3215c0d79c31fb08a42
Yetbarek Temesgen <yetba_rek@hotmail.com>
2025-12-06 18:28:45 -0500

    Vercel-ready Vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Vite's default output directory
+  },
   server: {
-    port: 5173
-  }
+    port: 5173, // optional, useful for local dev
+    strictPort: true,
+  },
 });
