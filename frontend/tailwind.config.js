aca332441ad8237d3050cd34a6c5323b9b90ac58
Yetbarek Temesgen <yetba_rek@hotmail.com>
2025-12-06 19:46:58 -0500

    Fix tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,11 @@
-// tailwind.config.js
-export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx}"],
-  darkMode: 'class',
-  theme: { extend: {} },
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
-};
+}
