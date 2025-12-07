7415a0ee2bfa2ddef4651179125b2aa796f86859
Yetbarek Temesgen <yetba_rek@hotmail.com>
2025-12-07 12:53:37 -0500

    Fix folder structure for backend/frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Vite's default output directory
-  },
-  server: {
-    port: 5173, // optional, useful for local dev
-    strictPort: true,
-  },
+    rollupOptions: {
+      external: ["axios"]
+    }
+  }
 });
